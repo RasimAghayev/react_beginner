c183c2d5dfa80739764029703e98ae69f21e55a7
Rasim Aghayev <rasimaqayev@gmail.com>
2022-02-20 13:42:55 +0400

    Comments list

---
+++ cmd
@@ -0,0 +1,2 @@
+npx create-react-app feedback-app --use-npm
+cd feedback-app
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    const title='Blog Post'
+    const body='This is m blog post'
+    const comments=[
+        {id:1,text: 'Comment 1'},
+        {id:2,text: 'Comment 2'},
+        {id:3,text: 'Comment 3'},
+        {id:4,text: 'Comment 4'},
+        {id:5,text: 'Comment 5'},
+    ]
+    const loading=false
+    const showComments=true
+    if(loading) return <h1>Loading....</h1>
+    const commentBlock=(
+        <div className='comments'>
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment,index)=>(
+                    <li key={index}>
+                        {comment.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+    // return (
+    //     <div>
+    //         <h1>Hello from the app component</h1>
+    //         <p>Rasim Aghayev</p>
+    //     </div>
+    // )
+    // return React.createElement(
+    //     'div',
+    //     {className:'container'},
+    //     React.createElement(
+    //         'h1',
+    //         {},
+    //         'My App'
+    //         )
+    // )
+    return (
+        <div className='container'>
+            <h1>{title}</h1>
+            <p>{body}</p>
+            {showComments&&commentBlock}
+        </div>
+    )
 }
-
-export default App;
+export default App
